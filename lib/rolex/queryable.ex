dbdcd22438324486c502d9c5d75378ce42560321
Brian Langenfeld <brian@knotfield.com>
2024-09-02 23:44:05 -0500

    Add typespec to Queryable module

--- lib/rolex/queryable.ex
+++ lib/rolex/queryable.ex
@@ -8,12 +8,16 @@ defmodule Rolex.Queryable do
   alias Rolex.DSL
   alias Rolex.Permission
 
+  @type any_role_opt :: DSL.any_role_opt()
+  @type any_to_opt :: DSL.any_to_opt()
+  @type any_on_opt :: DSL.any_on_opt()
+
   @doc """
   Scopes `query` to records that are the subject ("who") of a granted permission.
 
-  Permission scope itself is controlled by `opts`.
+  Role and object scope are narrowed by DSL options.
 
-  ## DSL
+  ## Options
 
     * `:role` - names the granted role
     * `:on` - the permission object ("what")
@@ -23,14 +27,15 @@ defmodule Rolex.Queryable do
       # users where :some_role is granted on a particular task
       iex> from(u in User) |> where_granted_to(role: :some_role, on: task)
   """
-  def where_granted_to(%Ecto.Query{} = query, opts \\ []) do
-    where_granted(query, opts, :subject_id)
+  @spec where_granted_to(Ecto.Queryable.t(), [any_role_opt() | any_on_opt()]) :: Ecto.Query.t()
+  def where_granted_to(queryable, opts \\ []) do
+    where_granted(queryable, opts, :subject_id)
   end
 
   @doc """
   Scopes `query` to records that are the object ("what") of a granted permission.
 
-  Permission scope itself is controlled by `opts`.
+  Role and subject scope are narrowed by DSL options.
 
   ## Options
 
@@ -42,11 +47,12 @@ defmodule Rolex.Queryable do
       # tasks where :some_role is granted to a particular user
       iex> from(t in Task) |> where_granted_on(role: :some_role, to: user)
   """
-  def where_granted_on(%Ecto.Query{} = query, opts \\ []) do
-    where_granted(query, opts, :object_id)
+  @spec where_granted_on(Ecto.Queryable.t(), [any_role_opt() | any_to_opt()]) :: Ecto.Query.t()
+  def where_granted_on(queryable, opts \\ []) do
+    where_granted(queryable, opts, :object_id)
   end
 
-  defp where_granted(%Ecto.Query{} = query, opts, id_field) do
+  defp where_granted(queryable, opts, id_field) do
     params =
       DSL.changeset_for_filter(opts)
       |> DSL.to_permission_params()
@@ -56,7 +62,7 @@ defmodule Rolex.Queryable do
       |> Permission.where_granted(params)
       |> distinct([p], field(p, ^id_field))
 
-    from(q in query,
+    from(q in queryable,
       inner_join: p in subquery(permissions),
       on: is_nil(field(p, ^id_field)) or field(p, ^id_field) == q.id
     )
